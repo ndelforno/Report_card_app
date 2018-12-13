8b190644c9e47f42dc48c6f463b92aa8342585ed
Ndelforno <nicolas.delforno@gmail.com>
2018-12-12 19:27:22 -0500

    Teacher name added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'students#index'
   get 'students/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
